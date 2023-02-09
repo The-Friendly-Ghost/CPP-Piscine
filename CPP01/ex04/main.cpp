f66dc8ee61f941950fa77a54ad86f7f1862b10d7
Casper Post <github-contact@capo.mozmail.com>
2023-02-09 16:00:54 +0100

    cpp01/ex04 done

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cpost <cpost@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/07 14:28:48 by cpost         #+#    #+#                 */
-/*   Updated: 2023/02/07 17:48:29 by cpost         ########   odam.nl         */
+/*   Updated: 2023/02/09 15:54:04 by cpost         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <string>
 #include <iostream>
 #include <sys/stat.h>
+#include <sstream>
 
 static inline bool	file_exists(char *str)
 {
@@ -22,40 +23,64 @@ static inline bool	file_exists(char *str)
 	return (stat(str, &buffer) == 0);
 }
 
-static inline void copy_infile_to_outfile(std::fstream &fs,
-		std::ofstream &outfile)
+static inline std::string	copy_infile_to_string(std::ifstream &fs)
 {
-	std::string	line;
+	std::stringstream	buffer;
 
-	while (getline(fs, line))
-		outfile << line << '\n';
+	buffer << fs.rdbuf();
+	return (buffer.str());
 }
 
 int	main(int argc, char **argv)
 {
-	std::fstream			fs;
+	std::ifstream			fs;
 	std::ofstream			outfile;
-	const std::string		fileExtension = ".replace";
+	std::string				filename;
+	std::string				total;
+	std::string				find;
+	std::string				replace;
 	size_t					pos;
 
+	//Check if amount of arguments is correct
 	if (argc != 4)
 	{
 		std::cout << "Wrong amount of arguments" << std::endl;
 		return (0);
 	}
+	
 	if (file_exists(argv[1]))
 	{
-		fs.open(argv[1]);
-		outfile.open(argv[1] + fileExtension);
-		copy_infile_to_outfile(fs, outfile);
-		// pos = outfile.find(argv[3]);
+		//Convert char* to std::string
+		filename = argv[1];
+		find = argv[2];
+		replace = argv[3];
 		
-		// while (pos != std::string::npos)
-		// {
-			
-		// 	pos = line.find(argv[3] + pos + 1);
-		// }
+		//open in- and output files
+		fs.open(filename);
+		outfile.open(filename + ".replace");
+		if (!fs || !outfile)
+		{
+			std::cout << "File opening failed" << std::endl;
+			return (0);
+		}
+		
+		//Copy content of infile to a string 'total'
+		total = copy_infile_to_string(fs);
 
+		//Find first occurence occurences of the 'find' in the 'total', and
+		// replace them with 'replace'
+		pos = total.find(find);
+		while (pos != std::string::npos)
+		{
+			total.erase(pos, find.length());
+			total.insert(pos, replace);
+			pos = total.find(find, pos + replace.length());
+		}
+
+		//Move total string to outfile
+		outfile << total;
+
+		//Close in- and outfile
 		outfile.close();
 		fs.close();
 	}
