e8d884061f1db767601e73ff63fdafe7a5d5804b
Casper Post <github-contact@capo.mozmail.com>
2023-03-10 19:38:59 +0100

    CPP05 ex02 in progress

---
+++ CPP05/ex02/AForm.hpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   AForm.hpp                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cpost <cpost@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/03/09 17:26:00 by cpost         #+#    #+#                 */
+/*   Updated: 2023/03/10 14:27:45 by cpost         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AFORM_HPP
+#define AFORM_HPP
+#include <string>
+#include "Bureaucrat.hpp"
+
+// Declaring Bureaucrat Class, because Ubuntu...
+class   Bureaucrat;
+
+class AForm
+{
+private:
+    const std::string   mName;
+    bool                mSigned;
+    const int           mSignGrade;
+    const int           mExecGrade;
+
+public:
+    virtual void abstractClass() = 0;
+
+    AForm(void);
+    AForm(std::string name, const int signGrade, const int execGrade);
+    AForm(const AForm &copy);
+    AForm    &operator=(const AForm &rhs);
+    ~AForm();
+
+    std::string   getFormName(void) const;
+    void          setSigned(void);
+    bool          getSigned(void) const;
+    int           getSignGrade(void) const;
+    int           getExecGrade(void) const;
+
+    void          beSigned(const Bureaucrat &signing);
+
+    class   GradeTooHighException : public std::exception
+    {
+        public :
+            virtual const char *what(void) const throw();
+    };
+
+    class   GradeTooLowException : public std::exception
+    {
+        public :
+            virtual const char *what(void) const throw();
+    };
+};
+
+std::ostream    &operator<<(std::ostream &out, AForm &rhs);
+
+
+
+
+#endif
---
+++ CPP05/ex02/Bureaucrat.cpp
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Bureaucrat.cpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cpost <cpost@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/03/09 13:41:31 by cpost         #+#    #+#                 */
+/*   Updated: 2023/03/10 14:03:25 by cpost         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+#include <iostream>
+#include <string>
+
+//=====================================//
+//=== Constructors / Deconstructor ====//
+//=====================================//
+
+Bureaucrat::Bureaucrat(void) :
+    mName("Visitor"),
+    mGrade(150)
+{
+    std::cout << "Bureaucrat Visitor Constructor Called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) :
+    mName(name),
+    mGrade(grade)
+{
+    std::cout << "Bureaucrat Constructor Called" << std::endl;
+    if (mGrade < 1)
+        throw (GradeTooHighException());
+    if (mGrade > 150)
+        throw (GradeTooLowException());
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy) :
+    mName(copy.getName()),
+    mGrade(copy.getGrade())
+{
+    std::cout << "Bureaucrat Copy Constructor Called" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+    std::cout << "Deconstructor Called for Bureaucrat: " << this->mName << std::endl;
+}
+
+//=====================================//
+//======== Operator Overloads =========//
+//=====================================//
+
+Bureaucrat  &Bureaucrat::operator=(const Bureaucrat &rhs)
+{
+    std::cout << "Bureaucrat Assignment Operator Called" << std::endl;
+    this->mGrade = rhs.getGrade();
+    return (*this);
+}
+
+std::ostream    &operator<<(std::ostream &out, const Bureaucrat &rhs)
+{
+    out << rhs.getName() << ", bureaucrat grade: " << rhs.getGrade();
+    return (out);
+}
+
+//=====================================//
+//============= Exceptions ============//
+//=====================================//
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return ("Grade to High");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return ("Grade to Low");
+}
+
+//=====================================//
+//======== Getters / Setters ==========//
+//=====================================//
+
+int Bureaucrat::getGrade(void) const
+{
+    return (this->mGrade);
+}
+
+const std::string Bureaucrat::getName(void) const
+{
+    return (this->mName);
+}
+
+//=====================================//
+//================ Other ==============//
+//=====================================//
+
+void    Bureaucrat::incrementGrade(void)
+{
+    if (this->mGrade == 1)
+        throw (GradeTooHighException());
+    this->mGrade -= 1;
+}
+
+void    Bureaucrat::decrementGrade(void)
+{
+    if (this->mGrade == 150)
+        throw (GradeTooLowException());
+    this->mGrade += 1;
+}
+
+void    Bureaucrat::signForm(Form &toSign)
+{
+    try
+    {
+        toSign.beSigned(*this);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        std::cout << this->mName << " not authorised to sign ";
+        std::cout << toSign.getFormName() << std::endl;
+    }
+}
