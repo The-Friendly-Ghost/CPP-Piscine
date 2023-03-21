dfbcadc20b2e08144d23e4f1b9eebd239380aeb4
Casper <casperaronpost@protonmail.com>
2023-03-21 19:35:07 +0100

    ex01 in progress

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -1,32 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   iter.hpp                                           :+:    :+:            */
+/*   main.cpp                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: cpost <cpost@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/03/21 15:07:43 by cpost         #+#    #+#                 */
-/*   Updated: 2023/03/21 19:34:46 by cpost         ########   odam.nl         */
+/*   Created: 2023/03/21 19:17:49 by cpost         #+#    #+#                 */
+/*   Updated: 2023/03/21 19:34:43 by cpost         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ITER_HPP
-#define ITER_HPP
+#include "iter.hpp"
+#include <string>
 
-#include <iostream>
-
-template<typename V>
-void    testFunction(T &value)
+int	main(void)
 {
-    std::cout << value << std::endl;
+	std::string	strArray[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+	int			intArray[] = {1, 2, 3, 4, 5, 6, 7};
+
+	iter(strArray, 7, testFunction<std::string>);
+	iter(intArray, 7, testFunction<int>);
 }
-
-template<typename T, typename N, typename F>
-void    iter(T *array, N length, F func)
-{
-    for (N i = 0; i < length; i++)
-        func(array[i]);
-}
-
-
-#endif
